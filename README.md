956164e322137d31900ffc01d14d0dd2c1b2d5de
javajohnhub <joedw1978@gmail.com>
2017-06-14 11:09:25 -0400

    tutorial and initial bot code

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
-# RAB
-Remote Apprentice Bot
+# Remote Apprentice Bot
+![Screenshot](https://s3.amazonaws.com/media.launchrock.com/assets/sites/site-bvrwu6f6280jiv2xrjws5zdaa/shobmz-citrudev_remoteapprentice.png)
+#### install node and npm using NVM
+
+```bash
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
+```
+or 
+
+```bash
+wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
+```
+then
+```bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+```
+#### install SSL using LetsEncrypt
+```bash
+$ sudo apt-get install software-properties-common
+$ sudo add-apt-repository ppa:certbot/certbot
+$ sudo apt-get update
+$ sudo apt-get install certbot
+```
+then
+```bash
+certbot certonly --standalone -d yoururl.com
+```
+then
+```bash
+certbot renew --dry-run
+```
+#### install PM2
+```bash
+npm install -g pm2
+```
+
+#### clone the repository
+```bash
+git clone https://github.com/Remote-Apprentice/RAB.git rab
+```
+#### start the server
+```bash
+cd rab
+pm2 start app.js
+```
+#### stop the server
+```bash
+pm2 stop app
+```
--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 var express = require('express')
 var fs = require('fs');
-
 var options = {
     key: fs.readFileSync('/etc/letsencrypt/live/remoteapprentice.io/privkey.pem'),
     cert: fs.readFileSync('/etc/letsencrypt/live/remoteapprentice.io/fullchain.pem')
 }
-
 var express = require('express');
 var app = express();
 var server = require('https').createServer(options, app)
+
 require('dotenv').config();
 
 
@@ -27,4 +26,83 @@ app.get('/', function(req, res) {
 
 server.listen(app.get('port'), app.get('ipaddr'), function(){
     console.log('Express server listening on  IP: ' + app.get('ipaddr') + ' and port ' + app.get('port'));
+});
+
+
+
+
+
+//Bot code can be moved to seperate file later
+
+var Botkit = require('../../lib/Botkit.js');
+
+
+if (!process.env.token) {
+    console.log('Error: Specify token in environment');
+    process.exit(1);
+}
+
+var controller = Botkit.slackbot({
+    debug: true
+});
+
+controller.spawn({
+    token: process.env.token
+}).startRTM(function(err) {
+    if (err) {
+        throw new Error(err);
+    }
+});
+
+
+controller.hears(['hello','hi'],['direct_message','direct_mention','mention'],function(bot,message) {
+    bot.reply(message,"Hello.");
+});
+
+controller.hears(['attach'],['direct_message','direct_mention'],function(bot,message) {
+
+    var attachments = [];
+    var attachment = {
+        title: 'This is an attachment',
+        color: '#FFCC99',
+        fields: [],
+    };
+
+    attachment.fields.push({
+        label: 'Field',
+        value: 'A longish value',
+        short: false,
+    });
+
+    attachment.fields.push({
+        label: 'Field',
+        value: 'Value',
+        short: true,
+    });
+
+    attachment.fields.push({
+        label: 'Field',
+        value: 'Value',
+        short: true,
+    });
+
+    attachments.push(attachment);
+
+    bot.reply(message,{
+        text: 'See below...',
+        attachments: attachments,
+    },function(err,resp) {
+        console.log(err,resp);
+    });
+});
+
+controller.hears(['dm me'],['direct_message','direct_mention'],function(bot,message) {
+    bot.startConversation(message,function(err,convo) {
+        convo.say('Heard ya');
+    });
+
+    bot.startPrivateConversation(message,function(err,dm) {
+        dm.say('Private reply!');
+    });
+
 });
