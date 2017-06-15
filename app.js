1b7d47399a4206f141bf7d23b141ca568816737e
javajohnhub <joedw1978@gmail.com>
2017-06-14 23:17:34 -0400

    test commit to see if server file update restarts server

--- app.js
+++ app.js
@@ -96,7 +96,7 @@ controller.hears(['attach'],['direct_message','direct_mention'],function(bot,mes
 
 controller.hears(['dm me'],['direct_message','direct_mention'],function(bot,message) {
     bot.startConversation(message,function(err,convo) {
-        convo.say('Heard ya');
+        convo.say('Heard ya loud and clear');
     });
 
     bot.startPrivateConversation(message,function(err,dm) {
