29a5fb116a913dcb683f3593ae3a26be423f1355
Evans <evansantonio32@gmail.com>
2017-06-19 01:55:35 +0000

    test log

--- botkit/modules/register.js
+++ botkit/modules/register.js
@@ -1,7 +1,7 @@
 module.exports = (handles, controller, bot) => {
     controller.hears(handles,
         'direct_message,direct_mention,mention', function(bot, message) {
-            console.log(message.user);
+            console.log(message);
            controller.storage.users.get(message.user, function (err, user) {
             if (user && user.name) {
                 bot.reply(message, user.name);
