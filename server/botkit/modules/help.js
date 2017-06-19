f27dc9cd86a866fed7882ab5ed7ccf8f4ebc96e5
Evans <evansantonio32@gmail.com>
2017-06-19 21:06:41 +0000

    Modified help attachment

--- server/botkit/modules/help.js
+++ server/botkit/modules/help.js
@@ -5,8 +5,8 @@ var reply_with_attachments = {
   'attachments': [
     {
       'fallback': 'To be useful, I need you to invite me in a channel.',
-      'title': 'Here is a list of commands I currently know:\n\n',
-      'text': '*uptime, identify yourself, who are you, ' + 
+      'title': 'Here is a list of commands I currently know:',
+      'text': '*\n\n uptime, identify yourself, who are you, ' + 
         'what is your name* - displays how long the bot has been running \n\n' +
         '*what is my name, who am i* - displays your username \n\n' +
         '*call me <your_name>, my name is <your_name>* - tell bot what your nickname is\n\n' +
