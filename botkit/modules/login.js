282185545c5899575283b8a12b56dacdd08bfd40
Evans <evansantonio32@gmail.com>
2017-06-20 00:30:06 +0000

    fixed files

--- botkit/modules/login.js
+++
@@ -1,6 +0,0 @@
-module.exports = (handles, controller, bot) => {
-    controller.hears(handles,
-        'direct_message,direct_mention,mention', function(bot, message) {
-            
-        });
-}
--- models/token.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose')
-
-var verificationTokenSchema = new mongoose.Schema({
-  _userId: {type: mongoose.Schema.Types.ObjectId, required: true, ref: 'User'},
-  token: {type: String, required: true},
-  createdAt: {type: Date, required: true, default: Date.now, expires: '1h'}
-})
-
-module.exports = mongoose.model('token', verificationTokenSchema)
