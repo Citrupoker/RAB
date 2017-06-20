97833a526c3258ce0272544a7d70343d2a5071cd
javajohnhub <joedw1978@gmail.com>
2017-06-20 19:02:54 -0400

    remove old attachment variable

--- server/botkit/index.js
+++ server/botkit/index.js
@@ -16,26 +16,7 @@ var controller = Botkit.slackbot({
 var bot = controller.spawn({
     token: process.env.token
 }).startRTM();
-
-var reply_with_attachments = {
-  'username': 'remoteapprentice' ,
-  'text': 'Help commands',
-  "mrkdwn": true,
-  'attachments': [
-    {
-      'fallback': 'To be useful, I need you to invite me in a channel.',
-      'title': 'Here is a list of commands I currently know',
-      'text': '*uptime, identify yourself, who are you,' 
-        + 'what is your name* - displays how long the bot has been running \n\n' +
-        '*what is my name, who am i* - displays your username \n\n' +
-        '*call me <your_name>, my name is <your_name>* - tell bot what your nickname is\n\n',
-      'color': '#7CD197',
-      'mrkdwn_in': ["text"],
-    }
-  ],
-  'icon_url': 'http://lorempixel.com/48/48'
-}
-
+    
 /*
     Include all chat bot modules here. Order matters.
     Note the "wHIte stripes" bug
