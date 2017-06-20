014451c51a255a12f1b2d85516bffd215dd818be
javajohnhub <joedw1978@gmail.com>
2017-06-20 16:33:27 -0400

    test to check for existing user

--- server/botkit/modules/register.js
+++ server/botkit/modules/register.js
@@ -1,8 +1,23 @@
+var mongoose = require('mongoose');
+var Members = require('./members');
+
 module.exports = (handles, controller, bot) => {
     controller.hears(handles,
         'direct_message', function(bot, message) {
             var email = message.match[1];
-
+            Members.schema.path('email').validate(function (value, res) {
+                Members.findOne({email: value}, 'id', function(err, user) {
+                    if (err) {
+                        return res(err);
+                    }
+                    if (user) {
+                        console.log('user line 14', user);
+                        return res(false);
+                    }
+                    console.log('user line 17', user);
+                    res(true);
+                });
+            }, 'already exists');
             //check email against db emails
 
             //if email is not already registered
