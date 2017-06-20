c704209087eab8fef5b79f7025f789fd0ec1579d
Evans <evansantonio32@gmail.com>
2017-06-20 15:45:42 +0000

    Added test API to check whether get request works on heroku server

--- server/api/index.js
+++ server/api/index.js
@@ -1,8 +1,13 @@
 
+var mainCtrl = require('../controllers')
 
 
-
-module.exports = function () {
+module.exports = function (app) {
     
+    app.post('/api/v1/addMembers', mainCtrl.addMember)
+    
+    app.get('/api/test', function(req, res) {
+        res.end('test success');
+    })
     
 }
