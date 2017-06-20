8675fbc21555f16c20c9e47bb79d64e0956e6087
javajohnhub <joedw1978@gmail.com>
2017-06-20 12:19:14 -0400

    finishing crud

--- server/api/index.js
+++ server/api/index.js
@@ -3,7 +3,10 @@ var mainCtrl = require('../controllers')
 
 
 module.exports = function (app) {
-    
+
+    app.get('/api/v1/members', mainCtrl.getMembers)
     app.post('/api/v1/addMembers', mainCtrl.addMember)
+    app.put('/api/v1/updateMember/:id', mainCtrl.updateMember)
+    app.delete('/api/v1/deleteMember/:id', mainCtrl.deleteMember)
     
 }
