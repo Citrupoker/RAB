ab76516f815d5f57922afcf7ef3d16c9a459894d
Evans <evansantonio32@gmail.com>
2017-06-20 21:38:07 +0000

    Testing register command

--- server/botkit/modules/register.js
+++ server/botkit/modules/register.js
@@ -1,22 +1,103 @@
+var mongoose = require('mongoose');
+var Members = require('../../models/members')
+
 module.exports = (handles, controller, bot) => {
     controller.hears(handles,
         'direct_message', function(bot, message) {
             var email = message.match[1];
-
+            
             //check email against db emails
+            Members.findOne({'email': email}, function(err, member) {
 
             //if email is not already registered
+                if (err) throw err
+                else if (member) {
+                    bot.reply(message, "You are already registered.");
+                } else {
             //add new user with email address
+                     var newMember = new Members({email: email});
             // trigger start of conversation
-            // Thank you. We need some information about you blah blah....
-
-            //What is your name?
-            //Write a brief description about yourself.
-            //What are your skills?
-            //Img url?
-            //personal website
-
-            //end conversation.
-    });
-
+                     bot.startPrivateConversation(message, function(err, convo) {
+                        if (!err) {
+                            convo.say('Thank you. We need some information about you');
+                            convo.ask('What is your name?', function(response, convo) {
+                                newMember.name = response.text;
+                                    convo.ask('Write a brief description about yourself.', function(response, convo) {
+                                        newMember.desc = response.text
+                                        convo.ask('Nice! Tell us about your skills? (Separate by commas.)', function(response, convo) {
+                                            newMember.skills = response.text.split(',');
+                                            convo.ask('Paste your image url here', function(response, convo) {
+                                                newMember.img = response.text;
+                                                convo.ask('Great! Now share the url to your personal website', function(response, convo) {
+                                                    newMember.website = response.text;
+                                                    convo.next();
+                                                });
+                                            });
+                                        });
+                                    });
+                            });
+                            convo.on('end', function(convo) {
+                                if (convo.status == 'completed') {
+                                    convo.say(message, 'Congratulations! You have been registered.');
+                                    
+                                    newMember.save(function(err, member) {
+                                       if (err) throw err;
+                                       bot.reply(message, format_attachment(member));
+                                       res.json(member);
+                                    });
+                                }
+                            })
+                        }
+                    })
+                }
+            });
+        })
 }
+
+function format_attachment(member) {
+    return {
+  'username': 'Remote Apprentice', 
+  "mrkdwn": true,
+          'attachments': [
+          {
+            'fallback':  'To be useful, I need you to invite me in a channel.',
+            'color': '#36a64f',
+            'pretext': member.name,
+            'author_icon': 'http://flickr.com/icons/bobby.jpg',
+            'title': member.website,
+            'title_link': member.website,
+            'text': member.desc,
+            'fields': [
+              {
+                'title': 'Name:',
+                'value': member.name,
+                'short': true
+
+              },
+              {
+                'title': 'Coach / Apprentice:',
+                'value': member.coach ? "Coach" : "Apprentice",
+                'short': true
+
+              },
+              {
+                'title': 'Skills:',
+                'value': member.skills.join(', '),
+                'short': true
+
+              },
+              {
+                'title': 'Roles:',
+                'value': member.roles.join(', '),
+                'short': true
+
+              }
+            ],
+            'image_url': member.img,
+            'thumb_url': 'http://example.com/path/to/thumb.png',
+            'footer': 'Profile',
+            'footer_icon': 'https://platform.slack-edge.com/img/default_application_icon.png'
+          }
+        ]
+    }
+} 
