f54133a5e11038fdc4694fcc414204eaf6beef01
Evans <evansantonio32@gmail.com>
2017-06-21 23:11:59 +0000

    Small change in update function

--- server/botkit/modules/update.js
+++ server/botkit/modules/update.js
@@ -23,28 +23,31 @@ module.exports = (handles, controller, bot) => {
                     bot.startPrivateConversation(message, function(err, convo) {
                         if(!err) {
                             convo.say('Okay, let\'s review your information.')
-                            convo.ask('What is your name? ' + 
-                            '*Current (please answer _yes_ to keep it this way):*\n\n>' + member.name, function(response, convo) {
-                                if (response.text.toLowerCase() !== 'yes') member.name = response.text
-                                convo.next()
-                            })
                             convo.ask('Please write a brief description about yourself. ' +
-                            '*Current (please answer _yes_ to keep it this way):*\n\n>' + member.desc, function(response, convo) {
+                            (member.desc && member.desc.length > 0) ? 
+                            '*Current (please answer _yes_ to keep it this way):*\n\n>' + member.desc : 
+                            '', function(response, convo) {
                                 if (response.text.toLowerCase() !== 'yes') member.desc = response.text
                                 convo.next()
                             })
                             convo.ask('Nice! Tell us about your skills. (Separate by commas.) ' + 
-                            '*Current (please answer _yes_ to keep it this way):*\n\n>' + member.skills.join(','), function(response, convo) {
+                            (member.skills && member.skills.length > 0) ? 
+                            '*Current (please answer _yes_ to keep it this way):*\n\n>' + member.skills.join(', ') : 
+                            '', function(response, convo) {
                                 if (response.text.toLowerCase() !== 'yes') member.skills = response.text.split(',')
                                 convo.next()
                             })
                             convo.ask('Okay. Now please paste your image url here. ' + 
-                            '*Current (please answer _yes_ to keep it this way):*\n\n>' + member.img, function(response, convo) {
+                            (member.img && member.img.length > 0) ? 
+                            '*Current (please answer _yes_ to keep it this way):*\n\n>' + member.img : 
+                            '', function(response, convo) {
                                 if (response.text.toLowerCase() !== 'yes') member.img = response.text
                                 convo.next()
                             })
-                            convo.ask('Great! Now share the url to your personal website. ' + 
-                            '*Current (please answer _yes_ to keep it this way):*\n\n>' + member.website, function(response, convo) {
+                            convo.ask('Great! Finally, share the url to your personal website. ' + 
+                            (member.website && member.website.length > 0) ? 
+                            '*Current (please answer _yes_ to keep it this way):*\n\n>' + member.website : 
+                            '', function(response, convo) {
                                 if (response.text.toLowerCase() !== 'yes') member.website = response.text
                                 member.save(function(err, member) {
                                     if(err) throw err
