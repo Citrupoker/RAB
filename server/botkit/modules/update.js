93d700768e27767d087c3fee1bcb5102890c19ca
Evans <evansantonio32@gmail.com>
2017-06-21 12:36:16 +0000

    Testing new update function

--- server/botkit/modules/update.js
+++ server/botkit/modules/update.js
@@ -2,56 +2,49 @@ var mongoose = require('mongoose');
 var Members = require('../../models/members')
 
 module.exports = (handles, controller, bot) => {
-    controller.hears(handles,
-        'direct_message', function(bot, message) {
-            var email = message.match[1];
-            
-            //check email against db emails
-            Members.findOne({'email': email}, function(err, member) {
-
-                //if email is not already registered
-                if (err) throw err
-                else if (member) {
-                    bot.reply(message, "You are already registered.");
-                } else {
-                     //add new user with email address
-                     var newMember = new Members();
-                    // trigger start of conversation
-                     bot.startPrivateConversation(message, function(err, convo) {
-                        if (!err) {
-                            convo.say('Thank you. We need some information about you.');
+    controller.hears(handles, 'direct_message', function(bot, message) {
+        var email = message.match[1]
+        
+        //Look for email to be updated in the database
+        Members.findOne({'email': email}, function(err, member) {
+            if(err) throw err
+            else if(!member) {
+                //If email is not in the database, notify the user they should register first
+                bot.reply(message, 'You haven\'t been registered yet. Please use the `register <your_email>` command to register.')
+            } else {
+                //If the email is in the database, start the conversation, asking the user for all the info
+                bot.startPrivateConversation(message, function(err, convo) {
+                    if(!err) {
+                        convo.say('Thank you. We need some information about you.')
                             convo.ask('What is your name?', function(response, convo) {
-                                newMember.name = response.text;
-                                convo.next();
-                            });
+                                member.name = response.text
+                                convo.next()
+                            })
                             convo.ask('Please write a brief description about yourself.', function(response, convo) {
-                                newMember.desc = response.text;
-                                convo.next();
-                            });
+                                member.desc = response.text
+                                convo.next()
+                            })
                             convo.ask('Nice! Tell us about your skills. (Separate by commas.)', function(response, convo) {
-                                newMember.skills = response.text.split(',');
-                                convo.next();
-                            });
+                                member.skills = response.text.split(',')
+                                convo.next()
+                            })
                             convo.ask('Okay. Now please paste your image url here.', function(response, convo) {
-                                newMember.img = response.text;
-                                convo.next();
-                            });
+                                member.img = response.text
+                                convo.next()
+                            })
                             convo.ask('Great! Now share the url to your personal website.', function(response, convo) {
-                                newMember.website = response.text;
-                            
-                                convo.say(message, 'Congratulations! You have been registered.');
-                                newMember.email = email;
-                                newMember.save(function(err, member) {
-                                   if (err) throw err;
-                                   bot.reply(message, format_attachment(member));
-                                });
-                            });
-                        
-                        }
-                    })
-                }
-            });
+                                member.website = response.text
+                                convo.say(message, 'Your information has been updated. Thank you.')
+                                member.save(function(err, member) {
+                                    if(err) throw err
+                                    bot.reply(message, format_attachment(member))
+                                })
+                            })
+                    }
+                })
+            }
         })
+    })
 }
 
 function format_attachment(member) {
