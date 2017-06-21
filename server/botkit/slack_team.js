c78a43a5371633eca31ba08e626eec2a0a1e28b3
javajohnhub <joedw1978@gmail.com>
2017-06-21 12:15:40 -0400

    typo

--- server/botkit/slack_team.js
+++ server/botkit/slack_team.js
@@ -1,7 +1,7 @@
 
 module.exports = () => {
     var rp = require('request-promise');
-    var url = `https://slack.com/api/user.list?token=${process.env.SLACK_OAUTH_TOKEN}&pretty=1`;
+    var url = `https://slack.com/api/users.list?token=${process.env.SLACK_OAUTH_TOKEN}&pretty=1`;
     var options = {
         uri: url,
         json: true
