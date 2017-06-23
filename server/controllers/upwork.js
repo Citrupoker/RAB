0b04099d81d393be32d641fe6743ee73868f6e83
javajohnhub <joedw1978@gmail.com>
2017-06-23 13:10:20 -0400

    try again upward api mock

--- server/controllers/upwork.js
+++ server/controllers/upwork.js
@@ -1,6 +1,6 @@
 var fs = require("fs");
 var contents = fs.readFileSync("upwork.json");
-var jobs = JSON.parse(contents);
+var jobs = JSON.parse(contents).replace(/u'(?=[^:]+')/g, "'");
 
 module.exports.getJobs = (req, res) => {
     console.log(jobs);
