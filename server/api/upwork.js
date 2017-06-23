0d54cfc051fdb0bc90dc75c3ac478356424d7a09
javajohnhub <joedw1978@gmail.com>
2017-06-23 13:52:14 -0400

    change get to post

--- server/api/upwork.js
+++ server/api/upwork.js
@@ -2,6 +2,6 @@ var upworkCtrl = require('../controllers/upwork');
 
 module.exports = function (app) {
 
-    app.get('/api/v1/upwork/jobs/:search', upworkCtrl.getJobs)
+    app.post('/api/v1/upwork/jobs/:search', upworkCtrl.getJobs)
 
 }
