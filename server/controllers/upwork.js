ddf91e5cdd9372daeb0d8103f220f5dff768c12d
javajohnhub <joedw1978@gmail.com>
2017-06-23 13:14:31 -0400

    mock

--- server/controllers/upwork.js
+++ server/controllers/upwork.js
@@ -1,6 +1,7 @@
 var fs = require("fs");
 var contents = fs.readFileSync("upwork.json");
-var jobs = JSON.parse(contents).replace(/u"(?=[^:]+')/g, '"');
+contents.replace(/u"(?=[^:]+')/g, '"');
+var jobs = JSON.parse(contents)
 
 module.exports.getJobs = (req, res) => {
     console.log(jobs);
