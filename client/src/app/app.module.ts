56e71922c759fa0ab9ecd4707c3660286773931f
javajohnhub <joedw1978@gmail.com>
2017-06-24 13:35:15 -0400

    added material design and progress bar

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,8 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+
+import {MdButtonModule, MdProgressBarModule} from '@angular/material'
+import 'hammerjs';
 
 @NgModule({
   declarations: [
@@ -12,7 +16,10 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    BrowserAnimationsModule,
+    MdButtonModule,
+    MdProgressBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
