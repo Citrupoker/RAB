7d246c35515918b50af39a144a92d0284a16a8bf
hamzicabdulah <hamzic@live.com>
2017-06-25 00:00:41 +0200

    Fixing images wrong path bug

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
     return (
       <div className='app'>
         <a href='https://slack.com/oauth/authorize?scope=incoming-webhook,commands,bot&client_id=188005782946.189109978850'>
-          <img src='img/add_to_slack.png' srcset='assets/img/add_to_slack.png 1x, assets/img/add_to_slack@2x.png 2x' alt='Add to Slack button' style={{margin: '0 auto', display: 'block'}} height='40' width='139'/>
+          <img src='img/add_to_slack.png' srcset='img/add_to_slack.png 1x, img/add_to_slack@2x.png 2x' alt='Add to Slack button' style={{margin: '0 auto', display: 'block'}} height='40' width='139'/>
         </a>
         <img src='img/ra.png' alt='Remote Apprentice Logo' className='img-responsive'/>
       </div>
