3039ed212bdc7ad7d0b9ac3a57c012d6c5723d3c
javajohnhub <joedw1978@gmail.com>
2017-06-24 19:51:56 -0400

    white stripes

--- server/botkit/index.js
+++ server/botkit/index.js
@@ -34,10 +34,9 @@ addHandler(['update'], "update")
 addHandler(['codepen (.*)'], "codepen")
 addHandler(['upwork (.*) (.*)'], 'upwork')
 addHandler(['uptime', 'identify yourself', 'who are you', 'what is your name'], "uptime")
-addHandler(['hello', 'hi'], "greetings")
 addHandler(['dm (.*) (.*) (.*)'], "twitterdm")
 addHandler(["profile"], "profile")
-
+addHandler(['hello', 'hi'], "greetings")
 function addHandler(handles, name){
      require("./modules/" + name + ".js")(handles, controller, bot)
 }
