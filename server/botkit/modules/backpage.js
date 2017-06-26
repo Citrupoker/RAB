af6e1275e8df4c9cd2a0d573010d5909cd20ce47
javajohnhub <joedw1978@gmail.com>
2017-06-26 09:07:58 -0400

    backpage

--- server/botkit/modules/backpage.js
+++ server/botkit/modules/backpage.js
@@ -10,13 +10,13 @@ module.exports = (handles, controller, bot) => {
     controller.hears(handles, 'direct_message,direct_mention,mention', function (bot, message) {
 
         console.log(message.match[1], message.match[2])
-        if(message.match[1] == 'backpage'){
+
             var city = encodeURIComponent(message.match[1]);
             var category = encodeURIComponent(message.match[2]);
             scraper.getLinks(`http://${city}.backpage.com/${category}`, (links) =>{
                 'use strict';
                 bot.reply(message, links)
             })
-        }
+        
     });
 }
