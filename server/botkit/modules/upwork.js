64d2a0cb8ce807c2dc56f03a73b6a9ee012425ea
javajohnhub <joedw1978@gmail.com>
2017-06-29 11:28:00 -0400

    changes for upwork api

--- server/botkit/modules/upwork.js
+++ server/botkit/modules/upwork.js
@@ -1,34 +1,31 @@
 module.exports = (handles, controller, bot) => {
-    /*
+
     var Upwork = require('upwork-api'),
         rl = require('readline'),
-        JobSearch = require('upwork-api/lib/routers/jobs/search.js').Search;
-
-    var config = {
-        'consumer_key': process.env.upworkKey,
-        'consumer_secret': process.env.upworkSecret
-    };
-
-    var api = new Upwork(config),
-        callbackUrl = 'callback url',
+        JobSearch = require('upwork-api/lib/routers/jobs/search.js').Search,
+        config = {
+        consumer_key: process.env.UPWORK_KEY,
+        consumer_secret: process.env.UPWORK_SECRET,
+      },
+        api = new Upwork(config),
+        callbackUrl = 'Https://remoteapprentice-dev.herokuapp.com/upwork',
         access = {};
 
-    api.getAuthorizationUrl(callbackUrl, function(error, url, requestToken, requestTokenSecret) {
+    api.getAuthorizationUrl(callbackUrl, function (error, url, requestToken, requestTokenSecret) {
         if (error) throw new Error('Can not get authorization url, error: ' + error);
 
         var interface = rl.createInterface(process.stdin, process.stdout);
-        interface.question('Please, visit ' + url + ' and enter a verifier: ', function(verifier) {
+        interface.question('Please, visit ' + url + ' and enter a verifier: ', function (verifier) {
             interface.close();
             process.stdin.destroy();
 
-            api.getAccessToken(requestToken, requestTokenSecret, verifier, function(error, accessToken, accessTokenSecret) {
+            api.getAccessToken(requestToken, requestTokenSecret, verifier, function (error, accessToken, accessTokenSecret) {
                 if (error) throw new Error(error);
                 access.token = accessToken;
                 access.tokenSecret = accessTokenSecret;
-            });
-        });
-    });
-    */
+              });
+          });
+      });
 
     controller.hears(handles, 'direct_message,direct_mention,mention', function (bot, message) {
         console.log(message.match[1], message.match[2]);
@@ -37,15 +34,14 @@ module.exports = (handles, controller, bot) => {
           bot.reply(message, 'Test success');
         }
 
-        /*
         var jobs = new JobSearch(api);
-        jobs.find({'title': search_term}, function(error, data) {
-            if(error) console.log(error);
+        jobs.find({ title: search_term }, function (error, data) {
+            if (error) console.log(error);
 
             data.forEach((job) => {
                 bot.reply(message, job.url);
-            });
-        });
-        */
+              });
+          });
+
       });
   };
