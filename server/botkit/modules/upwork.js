d335f13a1ec7f1c24346cce53efd9f251d1a0ec1
javajohnhub <joedw1978@gmail.com>
2017-06-29 11:45:27 -0400

    Revert "w6yu65e"
    This reverts commit 8fc79d3d46e8a6ac78973f3448c64db7cfce5bd1.

--- server/botkit/modules/upwork.js
+++ server/botkit/modules/upwork.js
@@ -7,7 +7,25 @@ module.exports = (handles, controller, bot) => {
         consumer_key: process.env.UPWORK_KEY,
         consumer_secret: process.env.UPWORK_SECRET,
       },
-        api = new Upwork(config);
+        api = new Upwork(config),
+        callbackUrl = 'Https://remoteapprentice-dev.herokuapp.com/upwork',
+        access = {};
+
+    api.getAuthorizationUrl(callbackUrl, function (error, url, requestToken, requestTokenSecret) {
+        if (error) throw new Error('Can not get authorization url, error: ' + error.error);
+
+        var interface = rl.createInterface(process.stdin, process.stdout);
+        interface.question('Please, visit ' + url + ' and enter a verifier: ', function (verifier) {
+            interface.close();
+            process.stdin.destroy();
+
+            api.getAccessToken(requestToken, requestTokenSecret, verifier, function (error, accessToken, accessTokenSecret) {
+                if (error) throw new Error(error);
+                access.token = accessToken;
+                access.tokenSecret = accessTokenSecret;
+              });
+          });
+      });
 
     controller.hears(handles, 'direct_message,direct_mention,mention', function (bot, message) {
         console.log(message.match[1], message.match[2]);
