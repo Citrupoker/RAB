f4b1d03d73c10069b0df8f005395ee42baa60cbd
Abdulah Hamzic <abdulahhamzic@gmail.com>
2017-08-06 13:40:54 +0000

    Notify new user when they post in introductions channel

--- server/botkit/modules/introduction.js
+++ server/botkit/modules/introduction.js
@@ -5,19 +5,19 @@ module.exports = (handles, controller, bot) => {
         if (message.channel === 'C5QGPRB52') {
             Member.findOne({ id: message.user }, (err, member) => {
                 if (err) throw err;
-                /*if (member) {
+                if (member.new) {
                     member.new = false;
                     member.save((err, member) => {
                         if (err) throw err;
                         console.log(member.name + ' is no longer a new member.');
-                    });*/
+                    });
                     bot.startPrivateConversation(message, function (err, convo) {
                         if (!err) {
                             convo.say('Welcome to our Slack team. Use the #FAQ channel to learn more about Remote Apprentice.');
                             convo.next();
                         }
                     });
-                //}
+                }
             });
         }
     });
