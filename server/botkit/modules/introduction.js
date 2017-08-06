1125a9c5203fb27f428c1da011777ae20305e954
Abdulah Hamzic <abdulahhamzic@gmail.com>
2017-08-06 13:23:12 +0000

    testing

--- server/botkit/modules/introduction.js
+++ server/botkit/modules/introduction.js
@@ -5,14 +5,14 @@ module.exports = (handles, controller, bot) => {
         if (message.channel === 'C5QGPRB52') {
             Member.findOne({ id: message.user }, (err, member) => {
                 if (err) throw err;
-                if (member.new) {
+                //if (member.new) {
                     member.new = false;
                     member.save((err, member) => {
                         if (err) throw err;
                         console.log(member.name + ' is no longer a new member.');
                     });
-                    bot.replyPrivate(message, 'Welcome to our Slack team. Use the #FAQ channel to learn more about Remote Apprentice.');
-                }
+                    bot.reply(message, 'Welcome to our Slack team. Use the #FAQ channel to learn more about Remote Apprentice.');
+                //}
             });
         }
     });
