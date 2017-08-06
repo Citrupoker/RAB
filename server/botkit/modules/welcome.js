2b809505e2c33d74a75c2822594dab658fee8266
Abdulah Hamzic <abdulahhamzic@gmail.com>
2017-08-06 15:23:43 +0000

    Changing channel join handler

--- server/botkit/modules/welcome.js
+++ server/botkit/modules/welcome.js
@@ -2,6 +2,7 @@ var Member = require('../../models/members');
 
 module.exports = (handles, controller, bot) => {
     controller.on(handles, function (bot, message) {
+        console.log('user joined');
         console.log(message);
     });
 };
