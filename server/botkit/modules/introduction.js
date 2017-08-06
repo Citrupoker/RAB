6d53b993115c320d677c1b49b1e2fcd50753eaed
Abdulah Hamzic <abdulahhamzic@gmail.com>
2017-08-06 13:34:52 +0000

    testing

--- server/botkit/modules/introduction.js
+++ server/botkit/modules/introduction.js
@@ -5,12 +5,12 @@ module.exports = (handles, controller, bot) => {
         if (message.channel === 'C5QGPRB52') {
             Member.findOne({ id: message.user }, (err, member) => {
                 if (err) throw err;
-                //if (member.new) {
+                /*if (member) {
                     member.new = false;
                     member.save((err, member) => {
                         if (err) throw err;
                         console.log(member.name + ' is no longer a new member.');
-                    });
+                    });*/
                     bot.startPrivateConversation(message, function (err, convo) {
                         if (!err) {
                             bot.say('Welcome to our Slack team. Use the #FAQ channel to learn more about Remote Apprentice.');
